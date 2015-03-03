6437e03f35c0e6dc31f8ede09567c2b21944f503
Ryan Cragun <me@ryan.ec>
2015-03-02 18:21:07 -0800

    Bump version and include test cookbook

---
+++ test/cookbooks/canaria-test/metadata.rb
@@ -0,0 +1,9 @@
+name             'canaria-test'
+maintainer       'Ryan Cragun'
+maintainer_email 'ryan@chef.io'
+license          'Apache 2.0'
+description      'Installs/Configures canaria-test'
+long_description 'Installs/Configures canaria-test'
+version          '0.1.0'
+
+depends 'canaria'
